523013d0b7591daaf0107a2a488d7ec4b9260c8d
MdHasanAliGHUN <mdhassanali01947@gmail.com>
2025-08-27 16:30:06 +0600

    implement heart count increase functionality and add call functionality

--- javaScript/js.js
+++ javaScript/js.js
@@ -1,17 +1,41 @@
 const coinSpan = document.getElementById("coin-count");
 const callHistoryContainer = document.getElementById("call-history-container");
 const callButtons = document.querySelectorAll(".call-btn");
+const heartCount = document.querySelector(".heartCount");
+const heartIcons = document.querySelectorAll(".heartIcon");
 
-for (const callButton of callButtons) {
-  callButton.addEventListener("click", () => {
-    let coins = parseInt(coinSpan.innerText);
-    if (coins <= 20) {
-      alert("Not enough coins!");
-    }
-    coins = coins - 20;
-    coinSpan.innerText = coins;
-    console.log(coins);
+function handleCall(e) {
+  let coins = parseInt(coinSpan.innerText);
 
-    
-  });
+  if (coins < 20) {
+    alert("Not enough coins!");
+    return;
+  }
+
+  coins -= 20;
+  coinSpan.innerText = coins;
+
+  const card = e.target.closest(".bg-white");
+  const title = card.querySelector(".cart-title").innerText;
+  const hotLineNumber = card.querySelector(".cart-hotline").innerText;
+
+  const callhistorybox = document.createElement("div");
+  callhistorybox.innerHTML = `
+      <h1>${title}</h1>
+      <p>${hotLineNumber}</p>
+  `;
+
+  callHistoryContainer.appendChild(callhistorybox);
 }
+
+callButtons.forEach((btn) => btn.addEventListener("click", handleCall));
+
+const increaseHeartCount = () => {
+  let primaryNumber = parseInt(heartCount.innerText);
+  primaryNumber++;
+  heartCount.innerText = primaryNumber;
+};
+
+heartIcons.forEach((icon) =>
+  icon.addEventListener("click", increaseHeartCount)
+);
