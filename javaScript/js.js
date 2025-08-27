5240943a87e4c0dada78818ec3fa98a7cab267f6
MdHasanAliGHUN <mdhassanali01947@gmail.com>
2025-08-27 17:15:54 +0600

    implement local time in call history

--- javaScript/js.js
+++ javaScript/js.js
@@ -3,7 +3,7 @@ const callHistoryContainer = document.getElementById("call-history-container");
 const callButtons = document.querySelectorAll(".call-btn");
 const heartCount = document.querySelector(".heartCount");
 const heartIcons = document.querySelectorAll(".heartIcon");
-const clearButton = document.getElementById("clearButton")
+const clearButton = document.getElementById("clearButton");
 
 function handleCall(e) {
   let coins = parseInt(coinSpan.innerText);
@@ -21,9 +21,24 @@ function handleCall(e) {
   const hotLineNumber = card.querySelector(".cart-hotline").innerText;
 
   const callhistorybox = document.createElement("div");
+  callhistorybox.classList =
+    "p-4 bg-white rounded-md border border-gray-200 my-3";
+
+  const now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes().toString().padStart(2, "0");
+  let seconds = now.getSeconds().toString().padStart(2, "0");
+
+  const time = `${hours} : ${minutes} : ${seconds}`;
+
   callhistorybox.innerHTML = `
-      <h1>${title}</h1>
-      <p>${hotLineNumber}</p>
+    <div class="flex justify-between items-center">
+      <div>
+        <h1 class="font-semibold text-lg my-1">${title}</h1>
+        <p class="font-semibold text-sm my-1">${hotLineNumber}</p>
+      </div>
+      <p class="text-sm">${time}</div>
+    </div>
   `;
 
   callHistoryContainer.appendChild(callhistorybox);
@@ -41,7 +56,6 @@ heartIcons.forEach((icon) =>
   icon.addEventListener("click", increaseHeartCount)
 );
 
-
 clearButton.addEventListener("click", () => {
-  callHistoryContainer.innerHTML = ""
-})
+  callHistoryContainer.innerHTML = "";
+});
