c9b66e70e37205be11fa649bddbf8ef223a670a9
MdHasanAliGHUN <mdhassanali01947@gmail.com>
2025-08-27 13:45:33 +0600

    Implement coin check and deduction for call buttons

--- javaScript/js.js
+++ javaScript/js.js
@@ -1,1 +1,17 @@
-console.log("Hi")
+const coinSpan = document.getElementById("coin-count");
+const callHistoryContainer = document.getElementById("call-history-container");
+const callButtons = document.querySelectorAll(".call-btn");
+
+for (const callButton of callButtons) {
+  callButton.addEventListener("click", () => {
+    let coins = parseInt(coinSpan.innerText);
+    if (coins <= 20) {
+      alert("Not enough coins!");
+    }
+    coins = coins - 20;
+    coinSpan.innerText = coins;
+    console.log(coins);
+
+    
+  });
+}
